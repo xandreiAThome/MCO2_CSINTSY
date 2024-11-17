6f75c45e96e3c282345232f1cd9a1f1aa0bb7e57
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-11-17 18:32:50 +0800

    finished the frontend part of the wumpus world

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const GRID = 5;
+var game = function () {
+  alert("You stepped on a pit, Game Over!");
+  location.reload();
+};
 // make child divs to all boxes and just assign robot class
 let program = `
 :- use_module(library(dom)).
 :- use_module(library(lists)).
+:- use_module(library(js)).
 :- dynamic(player/2), dynamic(grid_size/1), dynamic(breeze/2), dynamic(glitter/2), dynamic(safe/2), dynamic(home/2).
 
 % resets all dynamics
@@ -41,10 +46,9 @@ draw_robot(X,Y) :-
   nth0(0,A,F),        % And get the atom from the list
   nth0(0,B,G),        % Done because there is no conversion predicate from number to atom
   atom_concat(F, G, R),
-  get_by_id(R, D),
-  create('div', H),
-  add_class(H, 'robot'),
-  append_child(D, H).
+  atom_concat(R, '-child', P),
+  get_by_id(P, D),
+  add_class(D, 'robot').
 
 undraw_robot(X,Y) :-
   number_chars(X, A),
@@ -52,9 +56,9 @@ undraw_robot(X,Y) :-
   nth0(0,A,F),
   nth0(0,B,G),
   atom_concat(F, G, R),
-  get_by_id(R, D),
-  parent_of(C, D),
-  remove(C).
+  atom_concat(R, '-child', P),
+  get_by_id(P, D),
+  remove_class(D, 'robot').
 
 undraw(X,Y,IMG) :-
   number_chars(X, A),
@@ -80,8 +84,8 @@ action(w) :- % up movement
   ((Y1 - 1 < 0) -> Y2 = Y1; Y2 is Y1 - 1),
   retract(player(X1,Y1)),
   assertz(player(X1,Y2)),
-  land(X1,Y2),
-  draw_robot(X1, Y2).
+  draw_robot(X1, Y2),
+  land(X1,Y2).
 
 action(s) :- % down movement
   player(X1,Y1),
@@ -90,8 +94,8 @@ action(s) :- % down movement
   ((Y1 + 1 >= G) -> Y2 = Y1; Y2 is Y1 + 1),
   retract(player(X1,Y1)),
   assertz(player(X1,Y2)),
-  land(X1,Y2),
-  draw_robot(X1, Y2).
+  draw_robot(X1, Y2),
+  land(X1,Y2).
 
 action(a) :- % left movement
   player(X1,Y1),
@@ -99,8 +103,8 @@ action(a) :- % left movement
   ((X1 - 1 < 0) -> X2 = X1; X2 is X1 - 1),
   retract(player(X1,Y1)),
   assertz(player(X2,Y1)),
-  land(X2,Y1),
-  draw_robot(X2, Y1).
+  draw_robot(X2, Y1),
+  land(X2,Y1).
 
 action(d) :- % up movement
   player(X1,Y1),
@@ -109,8 +113,8 @@ action(d) :- % up movement
   ((X1 + 1 >= G) -> X2 = X1; X2 is X1 + 1),
   retract(player(X1,Y1)),
   assertz(player(X2,Y1)),
-  land(X2,Y1),
-  draw_robot(X2, Y1).
+  draw_robot(X2, Y1),
+  land(X2,Y1).
 
 % note: query for getting the element E at index I (zero index)
 % nth0(I, [a,b,c], E, _).
@@ -127,6 +131,9 @@ show_entities(X,Y) :-
   glitter(X,Y) -> draw(X,Y,'glitter'),
   breeze(X,Y) -> draw(X, Y, 'breeze').
 
+game_end(X) :-
+prop('game', G), apply(G, [0], H).
+
 land(X,Y) :- % land to X Y
 	% check the grid
 	grid(G),
@@ -134,23 +141,33 @@ land(X,Y) :- % land to X Y
   ((E == 2) -> (draw(X,Y,'breeze'), assertz(breeze(X,Y))); true),
   ((E == 4) -> (draw(X,Y,'glitter-and-breeze'), assertz(breeze(X,Y)), assertz(glitter(X,Y))); true),
   ((E == 1) -> (draw(X,Y,'glitter'), assertz(glitter(X,Y))); true),
+  ((E == 3) -> (draw(X,Y,'pit'), game_end(P)); true),
 
   % determine safety of current and adjacent
 	% spaces depending on what is known
-  (is_safe(X,Y); true).
-	%X2 is X - 1, Y2 is Y,
-	%is_safe(X,Y,X2,Y2).
-	%X3 is X, Y3 is Y + 1,
-	%is_safe(X,Y,X3,Y3), 
-	%X4 is X, Y4 is Y - 1,
-	%is_safe(X,Y,X4,Y4).
-	%(found_gold(X,Y);true)
+  (breeze(X,Y) -> true;  draw(X, Y, 'safe'), assertz(safe(X,Y)) ),
+  (is_safe(X,Y)).
 
 is_safe(X1,Y1) :- % space at X2, Y2 is safe if there is no breeze at X1, Y1
-  X2 is X1 + 1, Y2 is Y1,
-	(breeze(X1, Y1) -> true;
-  (draw(X2, Y2, 'safe'), draw(X1, Y1, 'safe'), assertz(safe(X1,Y1)), assertz(safe(X2,Y2))
-   ) ).
+	(breeze(X1, Y1) -> true; draw_safe_left(X1,Y1), draw_safe_right(X1,Y1), draw_safe_down(X1,Y1), draw_safe_up(X1,Y1)).
+
+draw_safe_left(X1,Y1) :-
+  X2 is X1 - 1,
+  (X2 >= 0) -> (draw(X2, Y1, 'safe'), assertz(safe(X2,Y1))); true.
+
+draw_safe_right(X1,Y1) :-
+  X2 is X1 + 1,
+  grid_size(G),
+  (X2 < G) -> (draw(X2, Y1, 'safe'), assertz(safe(X2,Y1))); true.
+
+draw_safe_down(X1,Y1) :-
+  Y2 is Y1 + 1,
+  grid_size(G),
+  (Y2 < G) -> (draw(X1, Y2, 'safe'), assertz(safe(X1,Y2))); true.
+
+draw_safe_up(X1,Y1) :-
+  Y2 is Y1 - 1,
+  (Y2 >= 0) -> (draw(X1, Y2, 'safe'), assertz(safe(X1,Y2))); true.
 
 maybe_pit(X,Y) :- % there may be a pit at X,Y if there is a breeze adjacent to it and not safe
 	A is X - 1,
@@ -169,6 +186,7 @@ init :-
   assertz(home(0,4)),
 	player(X,Y),
   draw(X,Y, 'home'),
+  land(X,Y),
   draw_robot(X,Y),
 	get_by_tag(body, Body),
 	bind(Body, keyup, _, clear_controls),
@@ -197,9 +215,12 @@ function makeGrid() {
   for (let y = 0; y < GRID; y++) {
     for (let x = 0; x < GRID; x++) {
       const pixel = document.createElement("div");
+      const r = document.createElement("div");
+      r.id = `${x}${y}-child`;
       pixel.classList.add("pixel");
       pixel.id = `${x}${y}`;
       map.appendChild(pixel);
+      pixel.appendChild(r);
     }
   }
 }
